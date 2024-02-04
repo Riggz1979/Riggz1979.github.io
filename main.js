76a2427fe50910d48a454bb35b6d48c167ffbaf2
msh <riggz1979@gmail.com>
2024-02-04 09:59:58 +0200

    Bot welcome page

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const btn = document.getElementById('change-background-btn');
 
+    btn.addEventListener('click', function () {
+        const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+        document.body.style.backgroundColor = randomColor;
+    });
+    btn.addEventListener('mouseover', function () {
+        btn.style.backgroundColor = '#005fa3'
+    })
+    btn.addEventListener('mouseout', function () {
+        btn.style.backgroundColor = '#0088cc'
+    })
+});
